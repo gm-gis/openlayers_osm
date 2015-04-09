2a0de7594fde8ba6e2c958b841dbf13ca7fc81fc
Greg Milbourne <gm-gis@users.noreply.github.com>
2015-04-09 18:01:32 -0400

    Update points.js

--- points.js
+++ points.js
@@ -2,13 +2,13 @@
 // http://bingmapsportal.com/ and use that instead.
 var apiKey = "AqTGBsziZHIJYYxgivLBf0hVdrAk9mWO5cQcb8Yux8sW5M8c8opEC2lZqKR1ZZXf";
 
-//var hybrid = new OpenLayers.Layer.Bing({
-//    key: apiKey,
-//    type: "AerialWithLabels",
-//    name: "Bing Aerial With Labels"
-//});
-var wms = new OpenLayers.Layer.WMS( "OpenLayers WMS",
-            "http://vmap0.tiles.osgeo.org/wms/vmap0", {layers: 'basic'} );
+var hybrid = new OpenLayers.Layer.Bing({
+    key: apiKey,
+    type: "AerialWithLabels",
+    name: "Bing Aerial With Labels"
+});
+//var wms = new OpenLayers.Layer.WMS( "OpenLayers WMS",
+  //          "http://vmap0.tiles.osgeo.org/wms/vmap0", {layers: 'basic'} );
 var vector = new OpenLayers.Layer.Vector("GeoJSON", {
     projection: "EPSG:4326",
     strategies: [new OpenLayers.Strategy.Fixed()],
@@ -18,7 +18,8 @@ var vector = new OpenLayers.Layer.Vector("GeoJSON", {
     })
 });
 
-var center = new OpenLayers.LonLat(-6793941.5586111,-4541765.4272672);
+
+var center = new OpenLayers.LonLat(-109.6, 46.7).transform("EPSG:4326", "EPSG:900913");).transform("EPSG:4326", "EPSG:900913");
 
 var map = new OpenLayers.Map({
     div: "map",
