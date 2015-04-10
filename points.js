17e4df658bd818fa10396d85a5e6bdf88b4aa046
Greg Milbourne <gm-gis@users.noreply.github.com>
2015-04-10 13:05:13 -0400

    Update points.js

--- points.js
+++ points.js
@@ -2,13 +2,13 @@
 // http://bingmapsportal.com/ and use that instead.
 var apiKey = "AqTGBsziZHIJYYxgivLBf0hVdrAk9mWO5cQcb8Yux8sW5M8c8opEC2lZqKR1ZZXf";
 
-var hybrid = new OpenLayers.Layer.Bing({
-    key: apiKey,
-    type: "AerialWithLabels",
-    name: "Bing Aerial With Labels"
-});
-//var wms = new OpenLayers.Layer.WMS( "OpenLayers WMS",
-  //          "http://vmap0.tiles.osgeo.org/wms/vmap0", {layers: 'basic'} );
+//var hybrid = new OpenLayers.Layer.Bing({
+//    key: apiKey,
+//    type: "AerialWithLabels",
+//    name: "Bing Aerial With Labels"
+//});
+var wms = new OpenLayers.Layer.WMS( "OpenLayers WMS",
+ /          "http://vmap0.tiles.osgeo.org/wms/vmap0", {layers: 'basic'} );
 var vector = new OpenLayers.Layer.Vector("GeoJSON", {
     projection: "EPSG:4326",
     strategies: [new OpenLayers.Strategy.Fixed()],
@@ -23,7 +23,7 @@ var center = new OpenLayers.LonLat(-60.24086515538559,-38.35180248916408).transf
 
 var map = new OpenLayers.Map({
     div: "map",
-    layers: [hybrid, vector],
+    layers: wms, vector],
     center: center,
     zoom: 8
 });
